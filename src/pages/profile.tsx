085f80d51e642ec18a58354c83c86699388e6006
kodaishiotsuki <0711dr5@gmail.com>
2023-05-07 23:30:03 +0900

    edit profile

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,19 +1,19 @@
+import { profile } from "@/types/profile";
 import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { FcEditImage } from "react-icons/fc";
 import Avatar from "../components/Avatar";
 import Card from "../components/Card";
 import Cover from "../components/Cover";
 import Layout from "../components/Layout";
-import ProfileContent from "../components/ProfileContent";
 import ProfileTabs from "../components/ProfileTabs";
-import { profile } from "@/types/profile";
 
 export default function ProfilePage() {
   const [profile, setProfile] = useState<profile | null>(null);
   const [editMode, setEditMode] = useState<boolean>(false);
-  const [name, setName] = useState("");
-  const [place, setPlace] = useState("");
+  const [name, setName] = useState<string>("");
+  const [place, setPlace] = useState<string>("");
   const router = useRouter();
   const userId = router.query.id;
   const tab = router?.query?.tab?.[0] || "posts";
@@ -22,16 +22,8 @@ export default function ProfilePage() {
   //ログインユーザーのプロフィールかどうか
   const isMyUser = session?.user?.id === userId;
 
-  //profileテーブルからユーザー情報を取得
-  useEffect(() => {
-    if (!userId) {
-      return;
-    }
-    fetchUser();
-  }, []);
-
-  //profileテーブルからユーザー情報を取得する関数
-  const fetchUser = () => {
+  //profileテーブルにユーザー情報を保存
+  const fetchUser = useCallback(() => {
     supabase
       .from("profiles")
       .select()
@@ -43,6 +35,32 @@ export default function ProfilePage() {
         if (result.data?.length) {
           setProfile(result.data[0]);
         }
+      });
+  }, [supabase, userId]);
+
+  //profileテーブルからユーザー情報を取得
+  useEffect(() => {
+    if (!userId) {
+      return;
+    }
+    fetchUser();
+  }, [fetchUser, userId]);
+
+  //profileテーブルにユーザー情報を保存
+  const saveProfile = () => {
+    supabase
+      .from("profiles")
+      .update({
+        name,
+        place,
+      })
+      .eq("id", session?.user.id)
+      .then((result) => {
+        //エラーがなければ、profileのnameとplaceを更新
+        if (!result.error) {
+          setProfile((prev) => ({ ...prev, name, place }));
+        }
+        setEditMode(false);
       });
   };
 
@@ -74,30 +92,73 @@ export default function ProfilePage() {
                   <div>
                     <input
                       type="text"
-                      className="border py-2 px-3 rounded-md"
-                      placeholder={"Your name"}
-                      onChange={(ev) => setName(ev.target.value)}
                       value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      className="border-2 border-gray-300 rounded-md p-1"
+                      placeholder="Your name"
                     />
                   </div>
                 )}
-                <h1 className="text-3xl font-bold">{profile?.name}</h1>
-
-                <div className="text-gray-500 leading-4">{profile?.place}</div>
+                {!editMode && (
+                  <h1 className="text-3xl font-bold">
+                    {!editMode && profile?.name}
+                  </h1>
+                )}
+                {!editMode && (
+                  <div className="text-gray-500 leading-4">
+                    {profile?.place || "internet"}
+                  </div>
+                )}
                 {editMode && (
                   <div>
                     <input
                       type="text"
-                      className="border py-2 px-3 rounded-md mt-1"
-                      placeholder={"Your location"}
-                      onChange={(ev) => setPlace(ev.target.value)}
                       value={place}
+                      onChange={(e) => setPlace(e.target.value)}
+                      className="border-2 border-gray-300 rounded-md p-1"
+                      placeholder="Your place"
                     />
                   </div>
                 )}
               </div>
+
               <div className="grow">
-                {/* <div className="text-right">
+                <div>
+                  {isMyUser && !editMode && (
+                    <button
+                      onClick={() => {
+                        setEditMode(true);
+                        setName(profile?.name!);
+                        setPlace(profile?.place!);
+                      }}
+                      className="inline-flex gap-2 items-center rounded-md shadow-sm shadow-gray-500 py-1 px-2"
+                    >
+                      <FcEditImage />
+                      Edit profile
+                    </button>
+                  )}
+
+                  {isMyUser && editMode && (
+                    <>
+                      <button
+                        onClick={saveProfile}
+                        className="inline-flex gap-2 items-center rounded-md shadow-sm shadow-gray-500 py-1 px-2"
+                      >
+                        Save profile
+                      </button>
+                      <button
+                        onClick={saveProfile}
+                        className="inline-flex gap-2 items-center rounded-md shadow-sm shadow-gray-500 py-1 px-2"
+                      >
+                        Cancel
+                      </button>
+                    </>
+                  )}
+                </div>
+              </div>
+
+              {/* <div className="grow">
+                <div className="text-right">
                   {isMyUser && !editMode && (
                     <button
                       onClick={() => {
@@ -140,8 +201,8 @@ export default function ProfilePage() {
                       Cancel
                     </button>
                   )}
-                </div> */}
-              </div>
+                </div>
+              </div> */}
             </div>
             <ProfileTabs active={tab} userId={profile?.id} />
           </div>
